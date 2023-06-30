6bc3e923b55952a49ee66c910413dcc23eef92ff
Wandrille Dioubaté <74004697+wandrilledioubate@users.noreply.github.com>
2023-06-30 10:53:01 +0200

    api

--- opini_api/api.py
+++ opini_api/api.py
@@ -1,45 +1,84 @@
-from fastapi import FastAPI, Query
 from scrapping import scrap
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request, jsonify
+import json
+import pandas as pd 
+import tensorflow_hub as hub
+from flask_cors import CORS, cross_origin
+import requests
+from bs4 import BeautifulSoup
+import re
+import random
+import csv
+import pandas as pd
+import numpy as np
+import tensorflow_hub as hub
+import keras.layers
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import tensorflow_text as text
 
-# from traitement import treat
-# from model_deep import deepModel
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Vous pouvez spécifier les domaines autorisés ici
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = Flask(__name__)
+cors = CORS(app)
 
 
-@app.post("/opini_ia")
-def process_data(url):
-    # Scrapper les données
-    data = scrap(url)
-    
-    """
-    # Traiter les données
-    treated_data = treat(data)
-    
-    # Effectuer le modèle de machine learning
-    result = deepModel(treated_data)
-    
-    # Formater la sortie
-    percentage = round(result * 100, 2)
-    output = {
-        "percentage": percentage,
-        "phrases_negatives": ["Phrase négative 1", "Phrase négative 2", "Phrase négative 3"],
-        "phrases_positives": ["Phrase positive 1", "Phrase positive 2", "Phrase positive 3"]
-    }
-    """
-    # return output
-    return data
+@app.route('/predict', methods=['POST'])
+def predict():
+    url = request.json['url']
+    scrap(url)
+    df_balanced = pd.read_csv('./data_comment.csv')
+    if 'Unnamed: 0' in df_balanced.columns:
+        df_balanced.drop(['Unnamed: 0'], axis=1, inplace=True)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    print(df_balanced)
+    # Convertir les labels en entiers avec LabelEncoder.
+    le = LabelEncoder()
+    labels_encoded = le.fit_transform(df_balanced['label_sentiment'])
 
-    
+    # Utiliser ces entiers pour créer les labels one-hot.
+    labels_one_hot = to_categorical(labels_encoded, num_classes=5)
+
+    X_train, X_test, y_train, y_test = train_test_split(df_balanced['text'], labels_one_hot, test_size= 0.3)
+    bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+    # bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4") 
+    new_model = keras.models.load_model("modelBERT_02.h5", custom_objects={'KerasLayer':hub.KerasLayer})
+    new_data = pd.read_csv("reviews.csv")
+    new_data_text = new_data['text']
+
+
+    # Preprocessing the text
+    # bert_inputs = bert_preprocess(new_data_text)
+
+    # Making the prediction
+    predictions = new_model.predict(new_data_text)
+
+    # Convertir les prédictions en labels lisibles
+    predicted_labels_encoded = np.argmax(predictions, axis=-1)
+    predicted_labels = le.inverse_transform(predicted_labels_encoded)
+
+    s = predicted_labels.mean()
+    m = s/len(predicted_labels) * 100
+
+    '''
+    phrases_df = pd.read_csv('./phrase.csv')
+
+
+    # Selectionner des phrases positives et négatives au hasard pour le type de lieu donné
+    positive_phrases = phrases_df[(phrases_df['type'] == type_lieu) & (phrases_df['sentiment'] == 'positif')]
+    negative_phrases = phrases_df[(phrases_df['type'] == type_lieu) & (phrases_df['sentiment'] == 'negatif')]
+
+    random_positive_phrases = positive_phrases.sample(3)['phrase'].tolist()
+    random_negative_phrases = negative_phrases.sample(3)['phrase'].tolist()
+    ''' 
+
+    return jsonify({
+        'Taux de satisfaction': m,
+        #'Points positifs': random_positive_phrases,
+        #'Points négatifs': random_negative_phrases
+    })  
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- opini_api/scrapping.py
+++ opini_api/scrapping.py
@@ -1,119 +1,38 @@
 import requests
-import asyncio
-import json
-import math
-from typing import TypedDict
-import httpx
-import re
 from bs4 import BeautifulSoup
-import os
+import re
+import random
+import csv
+import pandas as pd
+import numpy as np
+import tensorflow_hub as hub
+import keras.layers
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
-def extract_page_manifest(html):
-    data = re.findall(r"pageManifest:({.+?})};", html, re.DOTALL)
-    if data:
-        return json.loads(data[0])
-    else:
-        print("No pageManifest found in HTML")
-        return None
-
-def safe_get(dictionary, *keys):
-    for key in keys:
-        if dictionary is None:
-            return None
-        dictionary = dictionary.get(key)
-    return dictionary
-
-
-
-def extract_named_urql_cache(urql_cache: dict, pattern: str):
-    data = json.loads(next(v["data"] for k, v in urql_cache.items() if pattern in v["data"]))
-    return data
-
-class Review(TypedDict):
-    id: str
-    date: str
-    rating: str
-    title: str
-    text: str
-    votes: int
-    language: str
-
-def parse_reviews(html) -> Review:
-    page_data = extract_page_manifest(html)
-    if page_data is None:
-        print("No pageManifest found in HTML")
-        return []
-
-    page_data = extract_page_manifest(html)
-    review_cache = extract_named_urql_cache(page_data["urqlCache"]["results"], '"reviewListPage"')
-    parsed = []
-    for review in review_cache["locations"][0]["reviewListPage"]["reviews"]:
-        parsed.append(
-            {
-                "id": review["id"],
-                "date": review["publishedDate"],
-                "rating": review["rating"],
-                "title": review["title"],
-                "text": review["text"],
-                "votes": review["helpfulVotes"],
-                "language": review["language"],
-            }
-        )
-    return parsed
-
-async def scrape_hotel(url, session):
-    try:
-        first_page = await session.get(url)
-        first_page.raise_for_status()  # Cela lève une exception si le statut n'est pas 200
-    except httpx.HTTPStatusError:
-        print(f"URL {url} non fonctionnelle.")
-        return None
-
-    first_page = await session.get(url)
-
-
-    # Ajoutez le code de vérification ici
-
-    _review_page_size = 10
-    total_review_pages = int(_review_page_size)
-    review_urls = [
-        url.replace("-Reviews-", f"-Reviews-or{_review_page_size * i}-") for i in range(1, total_review_pages)
-    ]
-    assert len(set(review_urls)) == len(review_urls)
-    review_responses = await asyncio.gather(*[session.get(url) for url in review_urls])
-    reviews = []
-    for response in [first_page, *review_responses]:
-        reviews.extend(parse_reviews(response.text))
-
-    hotel_data = {
-        "reviews": reviews,
-    }
-    return hotel_data
-
-
-def scrape_reviews(url):
+def scrape_reviews_resto(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
     }
 
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Cela lève une exception si le statut n'est pas 200
-    except requests.HTTPError:
-        print(f"URL {url} non fonctionnelle.")
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while scraping {url}: {e}")
         return []
 
     reviews = []
-    for i in range(0, 100,20):
+    for i in range(0, 100, 100):  # Change this to change the number of pages you scrape
         page_url = f"{url}-or{i}"
         response = requests.get(page_url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
 
+        # Extract restaurant name and id
         restaurant_name_element = soup.find('h1', class_="HjBfq")
         restaurant_name = restaurant_name_element.text if restaurant_name_element is not None else 'Inconnu'
-        restaurant_id = url.split('-')[1].replace('d','')
-
         for review in soup.find_all('div', class_='review-container'):
             id_div = review.find('div', class_="reviewSelector")
             id_ = id_div.get('data-reviewid') if id_div else ''
@@ -121,67 +40,166 @@ def scrape_reviews(url):
             title = review.find('span', class_='noQuotes').text
             text = review.find('p', class_='partial_entry').text
             date = review.find('span', class_='ratingDate').get('title')
-            language_div = review.find('div', class_='translationOptions')
-            language = language_div.get('language') if language_div else ''
             votes_span = review.find('span', class_='thankButton hsx_thank_button')
             votes = votes_span.text.strip() if votes_span and votes_span.text.strip() else '0'
 
             reviews.append({
                 'id': id_,
                 'date': date,
-                'rating': rating,
+                'rating': int(int(rating) / 10),
                 'title': title,
                 'text': text,
                 'votes': votes,
-                'language': language,
-                'restaurant_name': restaurant_name,
-                'restaurant_id': restaurant_id
+                'name': restaurant_name
             })
 
     return reviews
 
-def save_to_json(data, filename):
-    with open(filename, 'w') as f:
-        if 'reviews' in data:
-            for item in data['reviews']:
-                item['rating'] = str(int(item['rating']))  # note hotel
-            json.dump(data['reviews'], f)
-        else:
-            for item in data:
-                item['rating'] = str(int(item['rating']) / 10)  # note resto
-            json.dump(data, f)
+
+def generate_random_id():
+    id_length = 9
+    random_id = ''.join(random.choices('0123456789', k=id_length))
+    return random_id
 
 
+def scrape_reviews_rental(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Une erreur s'est produite lors de la récupération des données à partir de {url}: {e}")
+        return []
+
+    reviews = []
+    page_url = url
+    while page_url:
+        response = requests.get(page_url, headers=headers, verify=False)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract rental name and id
+        rental_name_element = soup.find('h1', class_="biGQs _P fiohW uuBRH")
+        rental_name = rental_name_element.text if rental_name_element is not None else 'Inconnu'
+
+        for review in soup.find_all('div', class_="_c"):
+            review_id = generate_random_id()
+            if review.find('svg', class_="UctUV d H0"):
+                rating_element = review.find('svg', class_="UctUV d H0")
+            elif review.find('svg', class_="JXZuC d H0"):
+                rating_element = review.find('svg', class_="JXZuC d H0")
+            else:
+                rating_element = None
+
+            if rating_element:
+                rating = rating_element.get('aria-label').split(' ')[0]
+                # Reste du code
+            else:
+                rating = None
+            title = review.find('div', class_="biGQs _P fiohW alXOW NwcxK GzNcM ytVPx UTQMg RnEEZ uuBRH").text.strip()
+            text = review.find('div', class_="biGQs _P pZUbB KxBGd").text.strip()
+            div_content = soup.find('div', class_='biGQs _P pZUbB ncFvv osNWb').contents[0]
+            date_pattern = r'Written (\w+ \d{1,2}, \d{4})'
+            match = re.search(date_pattern, div_content)
+            if match:
+                date = match.group(1)
+            else:
+                date = None
+
+            reviews.append({
+                'id': review_id,
+                'date': date,
+                'rating': int(float(rating)),
+                'title': title,
+                'text': text,
+                'vote': 0,
+                'rental_name': rental_name,
+            })
+
+        next_button = soup.find('a', class_="_3fPsSAYi")
+        page_url = next_button['href'] if next_button else None
+
+    return reviews
 
 
-def delete_old_json_files():
-    files_to_delete = ['output.json']
-    for file in files_to_delete:
-        if os.path.exists(file):
-            os.remove(file)
-        else:
-            print(f"Le fichier {file} n'existe plus")
+def scrape_hotel(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Une erreur s'est produite lors de la récupération des données à partir de {url}: {e}")
+        return []
+
+    reviews = []
+    page_url = url
+    while page_url:
+        for i in range(0, 100, 100):  # Change this to change the number of pages you scrape
+            page_url = f"{url}-or{i}"
+            response = requests.get(page_url, headers=headers, verify=False)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Extract rental name and id
+            rental_name_element = soup.find('h1', class_="biGQs _P rRtyp")
+            rental_name = rental_name_element.text if rental_name_element is not None else 'Inconnu'
+
+            for review in soup.find_all('div', class_="YibKl MC R2 Gi z Z BB pBbQr"):
+                div_element = review.find('div', class_="WAllg _T")
+                review_id = int(div_element.get('data-reviewid'))
+                rating = (review.find('span', class_='ui_bubble_rating').get('class')[1].split('_')[1])
+                title_element = review.find('div', class_="KgQgP MC _S b S6 H5 _a")
+                title = title_element.find('span').text.strip()
+                text = review.find('span', class_="QewHA H4 _a").text.strip()
+                date_element = review.find('span', class_="teHYY _R Me S4 H3")
+                try:
+                    date_text = date_element.text.strip()
+                except:
+                    date_text = "01/01/2019"
+                try:
+                    date = date_text.split(':')[1].strip()
+                except IndexError:
+                    date = "01/01/2019"
+                vote = 0
+
+                reviews.append({
+                    'id': review_id,
+                    'date': date,
+                    'rating': int(int(rating) / 10),
+                    'title': title,
+                    'text': text,
+                    'vote': vote,
+                    'name': rental_name,
+                })
+
+            next_button = soup.find('a', class_="ui_button nav next primary ")
+            page_url = next_button['href'] if next_button else None
+
+    return reviews
 
 
-async def scrap(url):
-    if 'Restaurant' in url:
-        reviews = scrape_reviews(url)
-        save_to_json(reviews, 'output.json')
-    elif 'Hotel' in url:
-        limits = httpx.Limits(max_connections=5)
-        BASE_HEADERS = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "accept-language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
-            "accept-encoding": "gzip, deflate, br",
-        }
-        async with httpx.AsyncClient(limits=limits, timeout=httpx.Timeout(15.0), headers=BASE_HEADERS) as session:
-            result =  scrape_hotel(url, session)
-            if result is not None:  
-                save_to_json(result, 'output.json')
+def scrap(url):
+    if 'Restaurant_Review' in url:
+        reviews = scrape_reviews_resto(url)
+    elif 'Hotel_Review' in url:
+        reviews = scrape_hotel(url)
+    elif 'VacationRentalReview' in url:
+        reviews = scrape_reviews_rental(url)
+    else:
+        print("Invalid URL")
+        return
 
-"""
-if __name__ == "__main__":
-    asyncio.run(scrap('https://www.tripadvisor.com//Restaurant_Review-g58471-d866189-Reviews-Akasaka_Restaurant-Federal_Way_Washington.html'))
-    
-"""
+    if reviews:
+        with open('reviews.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=reviews[0].keys())
+            writer.writeheader()
+            writer.writerows(reviews)
+        print("Data saved successfully as CSV.")
+    else:
+        print("No reviews found.")
+
+
